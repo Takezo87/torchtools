dc2b4cf336f738ea0dabf2e847dc3af347f9a1f2
Takezo87 <barnabas87@gmx.de>
2020-01-20 15:12:06 +0100

    01 added

--- torchtools/_nbdev.py
+++ torchtools/_nbdev.py
@@ -5,9 +5,16 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"test": "00_core.ipynb",
          "leaky_loss": "00_core.ipynb",
          "unweighted_profit": "00_core.ipynb",
-         "weighted_profit": "00_core.ipynb"}
+         "weighted_profit": "00_core.ipynb",
+         "noop": "01_models.ipynb",
+         "shortcut": "01_models.ipynb",
+         "Inception": "01_models.ipynb",
+         "InceptionBlock": "01_models.ipynb",
+         "InceptionTime": "01_models.ipynb",
+         "InceptionTimeSgm": "01_models.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "models.py"]
 
 doc_url = "https://Takezo87.github.io/torchtools/"
 
